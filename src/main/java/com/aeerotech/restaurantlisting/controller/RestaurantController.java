986d46dff3bf69497e63bc2c9c9c87e20fbd2ecf
anand nerurkar <anand.nerurkar@gmail.com>
2024-12-12 13:56:51 +0530

    Initial commit

---
+++ src/main/java/com/aeerotech/restaurantlisting/controller/RestaurantController.java
@@ -0,0 +1,44 @@
+package com.aeerotech.restaurantlisting.controller;
+
+
+import com.aeerotech.restaurantlisting.dto.RestaurantDTO;
+import com.aeerotech.restaurantlisting.service.RestaurantService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/restaurant")
+@CrossOrigin
+public class RestaurantController {
+
+    @Autowired
+    RestaurantService restaurantService;
+
+
+    
+    @GetMapping
+    public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
+        List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
+        return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
+        RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB(restaurantDTO);
+        return new ResponseEntity<>(restaurantAdded, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id) {
+       return restaurantService.fetchRestaurantById(id);
+    }
+
+
+
+
+}
